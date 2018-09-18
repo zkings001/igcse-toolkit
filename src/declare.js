b8c915cddf5b64f9667f7e5448fcac83816dcf72
TheComputerM <mudit.harshit99@gmail.com>
2018-09-18 14:10:44 +0530

    Added menubar

--- src/declare.js
+++ src/declare.js
@@ -1,9 +1,10 @@
 var $ = require('jquery');
 var M = require('materialize-css');
 var production = false;
-var {shell} = require('electron');
+var {shell, remote} = require('electron');
 var fs = require('fs');
 var mudit = "This is an Easter Egg"
+var win = remote.getCurrentWindow()
 
 $(document).ready(() => {
     var floatingBtn = document.querySelector('.fixed-action-btn');
@@ -12,4 +13,22 @@ $(document).ready(() => {
     })
     var modal = document.querySelector('.modal');
     var modalInstance = M.Modal.init(modal, {});
-})
+});
+
+// Menu Buttons
+$('#win-maximize').click(() => {
+    console.log('Hello');
+    if(!win.isMaximized()){
+        win.maximize();
+    } else {
+        win.unmaximize();
+    }
+});
+
+$("#win-minimize").click(() => {
+    win.minimize();
+});
+
+$("#win-close").click(() => {
+    win.close();
+});
